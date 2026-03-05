a97105873b895674e985fb2e89375fdca20841e5
Cleber F. Carvalho <cleberfc23@gmail.com>
2026-03-05 18:29:13 -0300

    adding basic query: encounters by age group

--- database/queries_basic.sql
+++ database/queries_basic.sql
@@ -2,4 +2,14 @@
 SELECT  COUNT(*) AS total_encounters,
         COUNT(DISTINCT patient_nbr) AS unique_patients,
         ROUND(100*SUM(CASE WHEN readmitted <> 'NO' THEN 1 ELSE 0 END)/COUNT(*),1) AS readmission_rate_pct
-FROM encounters
+FROM encounters;
+
+-- 2. Encounters by Age Group
+SELECT
+    p.age,
+    COUNT(*) AS encounters
+FROM encounters e
+JOIN patients p 
+ON p.patient_nbr = e.patient_nbr
+GROUP BY p.age
+ORDER BY p.age;
